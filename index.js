8d8f1d7a8386ff7892775861b69776e36f0a57d8
Nanosoft Devs <dev@nanosoft.co.za>
2020-07-19 14:31:55 +0200

    Intial commit by simplegit

---
+++ index.js
@@ -0,0 +1,30 @@
+// const simpleGitPromise = require('simple-git');
+const simpleGitPromise = require('simple-git/promise')();
+
+
+const gitHubUrl = `git@github.com:nanosoftcoza/city.git`;
+// add local git config like username and email
+// simpleGitPromise.addRemote('origin', gitHubUrl);
+// Add all files for commit
+simpleGitPromise.add('.')
+    .then(
+        (addSuccess) => {
+            console.log(addSuccess);
+        }, (failedAdd) => {
+            console.log('adding files failed');
+        });
+// Commit files as Initial Commit
+simpleGitPromise.commit('Intial commit by simplegit')
+    .then(
+        (successCommit) => {
+            console.log(successCommit);
+        }, (failed) => {
+            console.log('failed commmit');
+        });
+// Finally push to online repository
+simpleGitPromise.push('origin', 'master')
+    .then((success) => {
+        console.log('repo successfully pushed');
+    }, (failed) => {
+        console.log('repo push failed');
+    });
